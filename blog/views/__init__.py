b0d32295a07b2fd571e235478e8287b692ebeeb2
Diego <diego.trabalhodev@gmail.com>
2025-12-21 18:20:08 -0300

    fazendo o template

--- blog/views/__init__.py
+++ blog/views/__init__.py
@@ -1,7 +1,4 @@
 from .post_views import PostView
+from .post_views import PostDetail
 from django.http import HttpResponse
 from django.views import generic
-
-class PostView(generic.View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Hello World')
